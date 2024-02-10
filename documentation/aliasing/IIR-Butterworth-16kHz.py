45c0fb2b50f44d354273e9e8605448a1435a47fa
twystd <twystd@users.noreply.github.com>
2024-02-10 13:41:44 -0800

    Calculating IIR coefficients for 16kHz Butterworth LPF

--- documentation/aliasing/IIR-Butterworth-16kHz.py
+++ documentation/aliasing/IIR-Butterworth-16kHz.py
@@ -22,17 +22,19 @@ for fs in FS:
         'a1': a[1],
         'a2': a[2],
         'a3': a[3],
+        'a4': a[4],
         'b0': b[0],
         'b1': b[1],
         'b2': b[2],
         'b3': b[3],
+        'b4': b[4],
     }
 
     print('    {')
     print('        %(fs)d,' % v)
     print('        IIR{')
-    print('            .a = {%(a1).6f, %(a2).6f, %(a3).6f},' % v)
-    print('            .b = {%(b0).6f, %(b1).6f, %(b2).6f, %(b3).6f}' % v)
+    print('            .a = {%(a1).6f, %(a2).6f, %(a3).6f, %(a4).6f},' % v)
+    print('            .b = {%(b0).6f, %(b1).6f, %(b2).6f, %(b3).6f, %(b4).6f}' % v)
     print('        },')
     print('    },')
 
